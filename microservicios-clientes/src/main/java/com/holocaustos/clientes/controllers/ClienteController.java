aa960fd663868286edfc379fbc6f6a04052d319a
deysi <deysisolorzano0301@gmail.com>
2025-03-25 11:15:43 -0600

    conexion de microservicio clientes, con frontend para envio de datos por medio del puerto 4200

--- microservicios-clientes/src/main/java/com/holocaustos/clientes/controllers/ClienteController.java
+++ microservicios-clientes/src/main/java/com/holocaustos/clientes/controllers/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,10 @@ import com.holocaustos.clientes.services.IClienteService;
 import jakarta.validation.Valid;
 
 @RestController
+
+//mandar a llama el puerto de angular, en donde enviaremos los datos
+@CrossOrigin(value = "http://localhost:4200") 
+
 public class ClienteController {
 
 	@Autowired
@@ -42,9 +47,9 @@ public class ClienteController {
 		return ResponseEntity.notFound().build();
 	}
 
-	@PostMapping
 	//public ResponseEntity<?> create(@Valid @RequestBody E entity, BindingResult result){
 
+	@PostMapping
 	public ResponseEntity<Cliente> create(@Valid @RequestBody Cliente entity, BindingResult result){
 		if(result.hasErrors()) {
 			return (ResponseEntity<Cliente>) this.validar(result);
